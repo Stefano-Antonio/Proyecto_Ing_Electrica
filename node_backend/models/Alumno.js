8c9456321d477671ab380ac36e53c83bb2eaca28
rogerzma <rogerzma500@gmail.com>
2025-02-19 13:29:25 -0600

    ligeros cambios

--- node_backend/models/Alumno.js
+++ node_backend/models/Alumno.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AlumnoSchema = new Schema({
+  id_carrera: { type: String, required: true },
   matricula: { type: String, required: true, unique: true },
   nombre: { type: String, required: true },
   telefono: { type: String, required: true },
