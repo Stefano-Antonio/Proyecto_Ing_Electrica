08b93f7d6356ba127a5026889c95e6eaf40382f4
Stefano-Antonio <stimpabo27@gmail.com>
2025-03-12 13:56:04 -0600

    Correcciones en coordinador y barra de busqueda en las faltatntes 2

--- react_frontend/src/components/AdministrarMaterias.js
+++ react_frontend/src/components/AdministrarMaterias.js
@@ -119,13 +119,13 @@ const AdministrarMaterias = () => {
                   <td>{materia.horarios.viernes || "-"}</td>
                   <td>
                     <button className="icon-button" onClick={() => handleModify(materia)}>
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="blue" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none" stroke="blue" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <path d="M12 20h9"></path>
                         <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
                       </svg>
                     </button>
                     <button className="icon-button">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="red" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none" stroke="red" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <polyline points="3 6 5 6 21 6"></polyline>
                         <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"></path>
                         <path d="M10 11v6"></path>
