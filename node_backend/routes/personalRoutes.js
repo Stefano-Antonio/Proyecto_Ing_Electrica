9de2931c56976ad1ddb9e97b32ba9f2ed22699cc
Stefano-Antonio <stimpabo27@gmail.com>
2025-03-17 17:04:41 -0600

    Resueltos conflictos de fusión en PersonalController.js y personalRoutes.js

--- node_backend/routes/personalRoutes.js
+++ node_backend/routes/personalRoutes.js
@@ -5,12 +5,14 @@ const personalController = require('../controllers/PersonalController');
 // Rutas para importar/exportar CSV
 router.post('/subir-csv', personalController.upload.single('csv'), personalController.subirPersonalCSV);
 router.get('/exportar-csv', personalController.exportarPersonalCSV);
-router.post('/', createPersonal);
-router.get('/', getPersonal);
-router.get('/:id', getPersonalById);
-router.put('/:id', updatePersonal);
-router.get('/carrera/:matricula', getPersonalByCarrera);
-router.delete('/:id', deletePersonal);
+
+router.post('/', personalController.createPersonal);
+router.get('/', personalController.getPersonal);
+router.get('/:id', personalController.getPersonalById);
+router.put('/:id', personalController.updatePersonal);
+router.get('/carrera/:matricula', personalController.getPersonalByCarrera);
+router.delete('/:id', personalController.deletePersonal);
+
 
 
 export default router;
