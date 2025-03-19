2b2ab2f1f5517e5d53dc79fb0e77c2f049fa8f84
rogerzma <rogerzma500@gmail.com>
2025-03-19 13:01:38 -0600

    correccion de un controlador

--- node_backend/routes/personalRoutes.js
+++ node_backend/routes/personalRoutes.js
@@ -5,6 +5,11 @@ const personalController = require('../controllers/PersonalController');
 // Rutas para importar/exportar CSV
 router.post('/subir-csv', personalController.upload.single('csv'), personalController.subirPersonalCSV);
 router.get('/exportar-csv', personalController.exportarPersonalCSV);
+
+//Rutas para importar/exportar CSV por carrera
+router.get("/exportar-csv/carrera/:id_carrera", personalController.exportarPersonalCSVPorCarrera);
+router.post("/subir-csv/carrera/:id_carrera", personalController.upload.single("csv"), personalController.subirPersonalCSVPorCarrera);
+
 router.post('/', personalController.createPersonal);
 router.get('/', personalController.getPersonal);
 router.get('/:id', personalController.getPersonalById);
@@ -14,4 +19,4 @@ router.delete('/:id', personalController.deletePersonal);
 
 
 
-export default router;
+module.exports = router;
