28612289d5a08dd2f0496c15cc4f30641437bb3c
rogerzma <rogerzma500@gmail.com>
2025-08-06 14:34:35 -0600

    40 horas por default para el coordinador de carrera, y solo un coordinador por carrera

--- node_backend/controllers/PersonalController.js
+++ node_backend/controllers/PersonalController.js
@@ -32,8 +32,16 @@ const storage = multer.diskStorage({
 // Crear personal y sus documentos relacionados
 exports.createPersonal = async (req, res) => {
     const { matricula, nombre, password, roles, correo, telefono, id_carrera } = req.body;
-
     try {
+
+        // Validar si ya hay un usuario de personal relacionado a un coordinador de carrera
+        if (roles === 'C' && matricula.match(/^C\d{4}$/)) {
+            const coordinadorExistente = await Coordinadores.findOne({ id_carrera });
+            if (coordinadorExistente) {
+                return res.status(409).json({ message: 'Ya existe un coordinador registrado para esta carrera.' });
+            }
+        }
+        
         const newPersonal = new Personal({ matricula, nombre, password, roles, correo, telefono });
         const usuarioGuardado = await newPersonal.save();
 
